4c091f8e39fee582ba00bcc7a30b8f0173c30fea
Sir-Williams <sirjohnwilliams@live.com>
2019-08-13 16:33:53 -0700

    added index vars and uses in index

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code away!
+const express = require('express');
+
+const server = express();
+
+const userRouter = require('./users/userRouter');
+const serverRouter = require('./server');
+
+
+server.use(express.json());
+
+server.use('/', serverRouter)
+server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter);
+
+server.listen(3000, () => console.log(`\nAPI RUNNING on port http://localhost:${port} \n`));
