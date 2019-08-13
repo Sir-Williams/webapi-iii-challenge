d47c1d7e40b0dc13b522c4fd095e6574d005b65e
Sir-Williams <sirjohnwilliams@live.com>
2019-08-13 16:46:52 -0700

    added console log to logger; added server uses; implamented router

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = 'express';
-
-const server = express();
+const router = require('./users/userRouter')
+const server = express()
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +9,13 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`[${new Date().toString()}] ${req.method} to ${req.url} from ${req.get('Origin',)}`)
+  
+  next();
 };
+
+server.use(express.json())
+server.use(logger)
+server.use('/users', router)
 
 module.exports = server;
